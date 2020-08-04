79e27acf63441a2484010319ce26225bb7fdba57
Full Name <maaar.iam94@gmail.com>
2020-08-04 20:19:54 -0300

    init repo

---
+++ src/components/PokeCell.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import sprites from '../assets/sprites.png';
+import './styles/PokeCell.css';
+
+const PokeCell = ({ pokeClass, handleOnClick }) => {
+    const { id, backgroundPosition } = pokeClass;
+    const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
+        return <button onClick={() => handleOnClick(id) } style={style} className="poke-cell"></button>
+};
+
+export default PokeCell;
